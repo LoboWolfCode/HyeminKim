f97edcb1d8202789d9c7babc55e20e66703212ff
Wolf's Code <lobowolfcode@protonmail.com>
2024-11-22 18:23:44 +0000

    first commit

---
+++ darkmode.js
@@ -0,0 +1,27 @@
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
+
+dom.addEventListener(`pointermove`,(e)=>{
+    dom.setAttribute(`controls`,true);
+   });
+   dom.addEventListener(`pointerout`,(e)=>{
+    dom.removeAttribute(`controls`);
+   });
